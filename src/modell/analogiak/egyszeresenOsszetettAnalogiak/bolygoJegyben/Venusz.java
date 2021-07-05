6247aab791b867c7baad44f60d35ebf758249e96
Molnar Adam <molnaradam8466@gmail.com>
2021-07-05 11:29:37 +0200

    Vénusz: kos, bika, ikrek, rák

---
+++ src/modell/analogiak/egyszeresenOsszetettAnalogiak/bolygoJegyben/Venusz.java
@@ -0,0 +1,424 @@
+package modell.analogiak.egyszeresenOsszetettAnalogiak.bolygoJegyben;
+
+public interface Venusz {
+
+    String[][][] venusz = new String[][][]{
+
+            /**
+            * A venusz mindig a holdat hozza letre , a venusz segiti a holdat , ea a mars segiti a nepot, viszont ha ez nem megy akkor forditott analogia
+             * jelenik meg es a mars analogiat a hold fogja tamogatni , a nap pedig a venuszt , igy keresztbe fordul meg.
+             *
+             * A lanyok egyrea marsikuasbbak, a ferfiak pedig egyre venusziasabbak(anyagi vagy szepseg fele mennek)
+             * Igy a ferfiak sokkal kevesbe a noben elik meg a venuszt ikabba a sajat penzugyeikben helyette
+             * Es a nok is sokkal kevesbe a ferfiakban elik a marsukat, helyette az akaratukban esa vagyaikban
+            * */
+
+
+            {//kos
+                    {//anyaghoz valo viszonya
+                        "küzd harcol érte",
+                            "nem akarja megtartani csak megszerezni",
+                            "keresi a kihívásokat, szeretne valamit, elég tudnia hogy megkaphatná és már nem kell neki",
+                            "sosem azt érzi, hogy az enyém, hanem hogy meg akaarom szerezni",
+                            "pozitiv megeles: a kihivast ne akarja beteljesiteni, legyen elég neki az, hogy elérhetné ",
+                    },
+                    {//biztonsagvagya
+                        "kockaztato tipus, egyaltalan nem a biztonsag neki a fontos",
+                            "nem érdekli, hogy biztonságban van-e, mert ha úgy gondolja megéri kockáztatni, meg fogja tenni"
+                    },
+                    {//stabilitas igenye
+                        "az induláshoz kell egy stabil pont, onnantól nem",
+                    },
+                    {//szepseghez valo visizonya
+                        "a szepseg a marsikussagat fejezi ki",
+                            "inkabb sportossagra torekszik",
+                            "vékonyság és izmosság, célszerűség",
+                            "nam túl lágyak"
+                    },
+                    {// nőiessége
+                            "a legférfiasabb nők, izmosak, markánsak, erősek",
+                            "neki pont a venusz es a hold terület sérül",
+                    },
+                    {// szexualitasa
+                        "kezdemenyező típus, nem nagyon befogadóak",
+                            "sokszor nem engedik a párjukat kezdeményezni"
+
+                    },
+                    {//művészete
+                        "marsikus művészetek: faragás. szobrászat, tűzzománc, ötvösség"
+                    },
+                    {//önelfogadása
+                        "riktán kerül szóba nála",
+                            "öntörvényőűek ebben, nem nagyon tesznek hangsúlyt a visszajelzésekre",
+
+                    }
+            },
+            {//bika
+                    {//altalanos
+                        "minél erősebben kötődjön az anyaghoz",
+                            "anyagi szinten ne tudjon változtani, hogy ragaszkodjon"
+                    },
+                    {//anyaghoz valo viszonya
+                        "nagyon fontos lesz a birtoklás",
+                            "fontos neki ,amit elér az életében tartsa meg",
+                            "neki minden anyag arra való hogy élevezze az életet(nem szabadna elfelejtenie)",
+                            "negyon nehezen mond le bármiről is"
+                    },
+                    {//biztonsagvagya
+                        "ez az ő adottsága, neki ez nagyon nagyon fontos",
+                            "ne mondjuk neki, hogy meg kell tanulnia elengedni",
+                            "tanulnia kell: a penz az anyag nem cél hanem eszköz",
+                            "ne azért birtokoljon valamit mert aza za enyém(céltalanul), hanem azért hogy az által valamit el tudjo érni"
+                    },
+                    {//stabilitas igenye
+                        "nagyon nagy, mindig stabil , biztonságos élethelyzetekre van szüksége",
+                            "mindig 4 lábbal álla földön, de ha egyet megemel már könnyen felborul",
+                            "nem szeret változtatni ezért igényli a stabilitást"
+                    },
+                    {//szepseghez valo visizonya
+                        "ösztönös szépérzéke van",
+                            "mindenben meglátja a szépet és a harmóniát",
+                            "természetesen szép, azt emeli ki magán ami van és nem azt ami nincs(merleg)",
+                            "gömbölyűség lágyság"
+                    },
+                    {// nőiessége
+                        "bajaság ösztönös",
+                            "legnőiesebb jegym ezért gömbölyűek, fogyókúra esélytelen",
+                            "ha nem fogadja el a testiségét pajzsmirigy problémája lesz"
+                    },
+                    {// szexualitasa
+                        "az egyik legjobb az ágyban , nagyon élvezkedős",
+                            "erős szexualitás a vágyak vezérlik, vele el lehet tölteni 1-2 napot az ágyban",
+                    },
+                    {//művészete
+
+                            "legjobb neki ha elkezd a művészetekkel foglalkozni",
+                            "övé az ösztönös művészet, bármelyik jó, de a legjobb az anyagszerű, fazekasság,szobrászat(formázás)"
+                    },
+                    {//önelfogadása
+                        "nagyon szeretné, ha mindenki hódolna neki",
+                    }
+            },{//ikrek
+                    {//altalanos
+                        "változékony avényusz",
+                            "nem ragaszkodik, nem kötődik, nem marad benne egy helyzetben",
+                            "ha nem mozog maatol, majd megmozditja a sors"
+                    },
+                    {//anyaghoz valo viszonya
+                        "van pénze, de folyamatos forgásban van",
+                            "az a legjobb ha nincs saját anya(másé minden)",
+                            "nem különösebben fontos neki, hogy valamit birtokoljon",
+                            "egyszer kötődne, máskor pont hogy nem kell neki, változik",
+                            "ha kötődik az anyaghoz, stabil élethelyzetben van szélhámos válik belőle",
+                            "olyan élethelyzetet kell teremtenie, hogy bármikor tovább tudjon lépni",
+                            "a pénzt szereti szórni"
+
+                    },
+                    {//biztonsagvagya
+                        "vagy jön valhonnan, vagy megy éppen valahova , útközben nincs biztonság",
+                            "mindig ott akarja magát biztonságban érezni ahol van",
+                            "kapcsolataikban szeretnek változtatni"
+                    },
+                    {//stabilitas igenye
+                        "semmi, nem keresi a stabil élethelyzeteket",
+                            "csak az legyen stabil , hogy valahonnan el tudjon indulni, és valahova tudjon érkezni",
+
+                    },
+                    {//szepseghez valo visizonya
+                        "nincs ízélese ehhez túlságosan",
+                            "nem azt veszi fel ami neki tetszik, hanem ami éppen szükséges azadott \"darabhoz\""
+                    },
+                    {// nőiessége
+                        "kislányosak, nem túl magasak , nem túl erősek, hanem ilyen serdulőlány formájúak",
+                    },
+                    {// szexualitasa
+                        "nem forróvérűek, de gyorsan tanulnak",
+                            "probléma, hogy nincs kötődés",
+                            "nehezen engedik el magukat"
+                    },
+                    {//művészete
+                        "színészet, szereplés, szavalás"
+                    },
+                    {//önelfogadása
+                        "nem nagyon tudja magát eldogadni, de ezzel nem is törődnik, neki ez nem fontos, hogy elfogadja- e magát",
+                            "inkább nem figyel oda az önelfogadásra, ezért olyan mintha elfogadná önmagát"
+                    }
+            },{//rák
+                    {//altalanos
+                        "lényeg az hogy érzelmileg kötődjön az anyagiakhoz",
+                            "ha érzelmileg kötődünk, sokkal nehezebben változtatnuk"
+                    },
+                    {//anyaghoz valo viszonya
+                        "érzelmileg közelíti meg a dolgokat, mindig az a döntő, hogy mit szeretek, mennyire kötősöm hozzá",
+                            "ők, akik, gyűjtik a gyerekek dolgait fényképeket stb, minden dolgot ami a kötődést segíti",
+                            "nem mond le semmi olyan dologról, ami érzelmileg megfogja őt",
+                    },
+                    {//biztonsagvagya
+                        "legaggodalmasabb jegy, mindig fél, hogy szerettei, családtagjai biztonságban vannak-e",
+                            "nagyon nehehzen viseli, ha az érzelmi stabilitása veszélybe kerül",
+                            "nem túl jók, a dolgok elengedésében"
+                    },
+                    {//stabilitas igenye
+                        "amiben érzelmei,vannak ott stabilitásra vágyik",
+                            "család, otthon érzelmi tárgyak stabilitására vágyik",
+                            "akkor tud változtatni, ha az érzelmei is változnak",
+
+                    },
+                    {//szepseghez valo visizonya
+                        "ne felejtse el, hiyg anikor gyerek van mcsalád van, neki valahol akkor is nőnek kell lennie",
+                            "szenvedélyesen szereti a laza kényelmes dolgokat",
+                            "nem divatkövető jegy, inkább az otthonosabb kényelmesebb dolgok jönnek be neki",
+                            "neki mindig az tetszik, ami érzelmi alapon nyújt valami többletet",
+                            "veszélye van az elhízásnak, ha jön a gyerek gömbölyödni fog utána"
+                    },
+                    {// nőiessége
+                        "ha figyelnek magukra nagyon nőiesek,ha nem akkor nem",
+                            "általában vonzzák a férfiakat, de neki ez azért kell, mert neki fontos a család és a gyerek",
+                            "nagyon odaadóak, nagyon szeretetteljesek és ezt a férfiak is szeretik",
+                            "azt hiszik, azzal fogják meg  a pasit, ha jók a konyhában",
+                            "azt gondolja, addig kell nőnek lenni amíg nem jön a gyerek"
+                    },
+                    {// szexualitasa
+                        "ha védekezik, csak idő kérdése, hogy a kapcsolatban tönkremenjen a szexualitás",
+                            "azért kapja a vénuszát , hogy hold legyen belőle",
+                            "ha azt akarja, hogy működjön a szex, ne védekezzen"
+                    },
+                    {//művészete
+                        "övék a népművészet, zenében cimbalom citera, stb, hímzés",
+                            "fontos neki, hogy a hagyományokat, a múltat ápolja"
+                    },
+                    {//önelfogadása
+                        "egyik legönbizalomhiányosabb, nem igazán tudja magát elfogadni",
+                            "ha család van akkor remekül el tudja fogadni magát"
+                    }
+            },{//oroszlán
+                    {//altalanos
+
+                    },
+                    {//anyaghoz valo viszonya
+
+                    },
+                    {//biztonsagvagya
+
+                    },
+                    {//stabilitas igenye
+
+                    },
+                    {//szepseghez valo visizonya
+
+                    },
+                    {// nőiessége
+
+                    },
+                    {// szexualitasa
+
+                    },
+                    {//művészete
+
+                    },
+                    {//önelfogadása
+
+                    }
+            },{//bika
+                    {//altalanos
+
+                    },
+                    {//anyaghoz valo viszonya
+
+                    },
+                    {//biztonsagvagya
+
+                    },
+                    {//stabilitas igenye
+
+                    },
+                    {//szepseghez valo visizonya
+
+                    },
+                    {// nőiessége
+
+                    },
+                    {// szexualitasa
+
+                    },
+                    {//művészete
+
+                    },
+                    {//önelfogadása
+
+                    }
+            },{//bika
+                    {//altalanos
+
+                    },
+                    {//anyaghoz valo viszonya
+
+                    },
+                    {//biztonsagvagya
+
+                    },
+                    {//stabilitas igenye
+
+                    },
+                    {//szepseghez valo visizonya
+
+                    },
+                    {// nőiessége
+
+                    },
+                    {// szexualitasa
+
+                    },
+                    {//művészete
+
+                    },
+                    {//önelfogadása
+
+                    }
+            },{//bika
+                    {//altalanos
+
+                    },
+                    {//anyaghoz valo viszonya
+
+                    },
+                    {//biztonsagvagya
+
+                    },
+                    {//stabilitas igenye
+
+                    },
+                    {//szepseghez valo visizonya
+
+                    },
+                    {// nőiessége
+
+                    },
+                    {// szexualitasa
+
+                    },
+                    {//művészete
+
+                    },
+                    {//önelfogadása
+
+                    }
+            },{//bika
+                    {//altalanos
+
+                    },
+                    {//anyaghoz valo viszonya
+
+                    },
+                    {//biztonsagvagya
+
+                    },
+                    {//stabilitas igenye
+
+                    },
+                    {//szepseghez valo visizonya
+
+                    },
+                    {// nőiessége
+
+                    },
+                    {// szexualitasa
+
+                    },
+                    {//művészete
+
+                    },
+                    {//önelfogadása
+
+                    }
+            },{//bika
+                    {//altalanos
+
+                    },
+                    {//anyaghoz valo viszonya
+
+                    },
+                    {//biztonsagvagya
+
+                    },
+                    {//stabilitas igenye
+
+                    },
+                    {//szepseghez valo visizonya
+
+                    },
+                    {// nőiessége
+
+                    },
+                    {// szexualitasa
+
+                    },
+                    {//művészete
+
+                    },
+                    {//önelfogadása
+
+                    }
+            },{//bika
+                    {//altalanos
+
+                    },
+                    {//anyaghoz valo viszonya
+
+                    },
+                    {//biztonsagvagya
+
+                    },
+                    {//stabilitas igenye
+
+                    },
+                    {//szepseghez valo visizonya
+
+                    },
+                    {// nőiessége
+
+                    },
+                    {// szexualitasa
+
+                    },
+                    {//művészete
+
+                    },
+                    {//önelfogadása
+
+                    }
+            },{//bika
+                    {//altalanos
+
+                    },
+                    {//anyaghoz valo viszonya
+
+                    },
+                    {//biztonsagvagya
+
+                    },
+                    {//stabilitas igenye
+
+                    },
+                    {//szepseghez valo visizonya
+
+                    },
+                    {// nőiessége
+
+                    },
+                    {// szexualitasa
+
+                    },
+                    {//művészete
+
+                    },
+                    {//önelfogadása
+
+                    }
+            },
+
+    };
+
+
+
+}
