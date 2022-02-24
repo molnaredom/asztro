bd780d970a0a44f1c1ae6ef78d8b23168644132e
molnar <molnar@inf.u-szeged.hu>
2022-02-24 09:28:04 +0100

    UPDATE: ML oldal bootstrap frontend

--- weboldal/base/views_updaters.py
+++ weboldal/base/views_updaters.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room2, Topic2, Jegy2, Bolygo2, Haz2, Message2 ,BolygoHazban2, BolygoJegyben2, HazJegyben2, Horoszkop2, HazUraHazban
-from django.contrib.auth.models import User
-from .forms import RoomForm, AnalogiaForm
-from django.contrib.auth import authenticate, login, logout
+from .models import Jegy2, Bolygo2, Haz2, BolygoHazban2, BolygoJegyben2, HazJegyben2, Horoszkop2, HazUraHazban
 from .forms import JegyekForm, BolygokForm, HazakForm , BolygoJegybenForm, HazJegybenForm, BolygoHazbanForm, RoomForm, HoroszkopForm, HazUraHazbanForm
 
 
-
-
-def updateAltalanos(request, nevID, AnalogiaObject, AnalogiaForm, visszateres_helye:str, rendereles_helye:str):
+def updateAltalanos(request, nevID, AnalogiaObject, AnalogiaForm, visszateres_helye:str):
     analogia = AnalogiaObject.objects.get(nevID=nevID)
     form = AnalogiaForm(instance=analogia)
 
@@ -25,9 +17,10 @@ def updateAltalanos(request, nevID, AnalogiaObject, AnalogiaForm, visszateres_he
             return redirect(visszateres_helye)
 
     context = {'form': form}
-    return render(request, f"create_templates/{rendereles_helye}", context)
+    return render(request, f"create_templates/analogia_form.html", context)
 
-def updateAltalanos_id(request, pk, AnalogiaObject, AnalogiaForm, visszateres_helye:str, rendereles_helye:str):
+
+def updateAltalanos_id(request, pk, AnalogiaObject, AnalogiaForm, visszateres_helye:str):
     analogia = AnalogiaObject.objects.get(id=pk)
     form = AnalogiaForm(instance=analogia)
 
@@ -38,7 +31,8 @@ def updateAltalanos_id(request, pk, AnalogiaObject, AnalogiaForm, visszateres_he
             return redirect(visszateres_helye)
 
     context = {'form': form}
-    return render(request, f"create_templates/{rendereles_helye}", context)
+    return render(request, f"create_templates/analogia_form.html", context)
+
 
 @login_required(login_url="login")
 def updateRestrichted(request, nevID, AnalogiaObject, AnalogiaForm, visszateres_helye:str, rendereles_helye:str):
@@ -59,34 +53,35 @@ def updateRestrichted(request, nevID, AnalogiaObject, AnalogiaForm, visszateres_
 
 
 def updateBolygo(request, nevID):
-    return updateAltalanos(request, nevID, Bolygo2, BolygokForm, "bolygok", "bolygok_form.html")
+    return updateAltalanos(request, nevID, Bolygo2, BolygokForm, "bolygok")
 
 
 def updateJegy(request, nevID):
-    return updateAltalanos(request, nevID, Jegy2, JegyekForm, "jegyek", "horoszkop_form.html")
+    return updateAltalanos(request, nevID, Jegy2, JegyekForm, "jegyek")
 
 
 def updateHaz(request, nevID):
-    return updateAltalanos(request, nevID, Haz2, HazakForm, "hazak", "hazak_form.html")
+    return updateAltalanos(request, nevID, Haz2, HazakForm, "hazak")
 
 
 def updateBolygoJegyben(request, id):
-    return updateAltalanos_id(request, id, BolygoJegyben2, BolygoJegybenForm, "bolygokJegyekben", "bolygoJegyben_form.html")
+    return updateAltalanos_id(request, id, BolygoJegyben2, BolygoJegybenForm, "bolygokJegyekben")
 
 
 def updateBolygoHazban(request, id):
-    return updateAltalanos_id(request, id, BolygoHazban2, BolygoHazbanForm, "bolygokHazakban", "bolygoHazban_form.html")
+    return updateAltalanos_id(request, id, BolygoHazban2, BolygoHazbanForm, "bolygokHazakban")
 
 
 def updateHazJegyben(request, id):
-    return updateAltalanos_id(request, id, HazJegyben2, HazJegybenForm, "hazakJegyekben", "hazJegyben_form.html")
+    return updateAltalanos_id(request, id, HazJegyben2, HazJegybenForm, "hazakJegyekben")
+
 
 def updateHazUraHazban(request, id):
-    return updateAltalanos_id(request, id, HazUraHazban, HazUraHazbanForm, "hazakUraHazakban", "hazUraHazban_form.html")
+    return updateAltalanos_id(request, id, HazUraHazban, HazUraHazbanForm, "hazakUraHazakban")
 
 
 def updateHoroszkop(request, id):
-    return updateAltalanos_id(request, id, Horoszkop2, HoroszkopForm, "horoszkop_gyujtemeny", "horoszkop_form.html")
+    return updateAltalanos_id(request, id, Horoszkop2, HoroszkopForm, "horoszkop_gyujtemeny")
 
 
 
