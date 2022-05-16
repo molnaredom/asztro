c03efeaf411fa55469cf5fdb25b69172f0618ab9
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-05-16 23:42:06 +0200

    UPDATE: mechine learning adatok nan értékek eltűntetnése

--- machine_learning/baseline/decision tree.py
+++ machine_learning/baseline/decision tree.py
@@ -1,5 +1,11 @@
-import pandas
 import pandas as pd
+import pandas
+from sklearn import tree
+import pydotplus
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+import matplotlib.image as pltimg
 
 from machine_learning.baseline.munka_osztalyozo import munka_osztalyozas
 
@@ -31,8 +37,45 @@ print(f"Munkákhoz rendelt horoszkópok száma {len(df)}")
 # minden munkához egy egy számértéket rendelünk
 osztalyozo = munka_osztalyozas(irodai=True)
 df['Munka'] = df['Munka'].map(osztalyozo)
-df.dropna(subset=['Munka']) # todo nem dobja el a nan ertekeket
-print(df)
+df.dropna(inplace=True) # inplace nelkul nem mukodik
 
 print(f"Osztályozott munkákhoz rendelt horoszkópok száma {len(df)}")
+
+print(df["Munka"].hist())
+
+labels = df.pop("Munka")
+features = df
+
+# print(labels)
+# print(features)
+data_top = df.head()
+
+
+
+feature_names = list(df.columns)
+
+categorical_adatok = feature_names
+categorical_adatok.remove("Életkor")
+# print(categorical_adatok)
+
+df[categorical_adatok] =\
+    df[categorical_adatok].astype('category')
+#
+# pd.get_dummies(data=data, columns=['Name'])
+
+df.apply(pd.to_numeric)
+#
+# print(features.dtypes)
+
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(features, labels)
+data = tree.export_graphviz(dtree, out_file=None, feature_names=feature_names)
+graph = pydotplus.graph_from_dot_data(data)
+graph.write_png('mydecisiontree.png')
+
+img=pltimg.imread('mydecisiontree.png')
+imgplot = plt.imshow(img)
+plt.show()
+
 
