f64e49d16bf1da3f9b306877e7ae8a2fa6247aa0
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-05-25 16:17:12 +0200

    REFACTORING: konkret analogia sema rahuzasa az adatbazis konkret elemeire

--- machine_learning/baseline/SGD_classifier.py
+++ machine_learning/baseline/SGD_classifier.py
@@ -6,60 +6,46 @@ from sklearn.linear_model import SGDClassifier
 
 
 def sgd_classifier(train_features, valid_features, train_classlabels, valid_classlabels):
-    model = SGDClassifier()
-    print(train_features)
 
     model_param = {
         'penalty': ['l2', 'l1', 'elasticnet'],
-        'l1_ratio': [0.0, 0.05, 0.1, 0.2, 0.5, 0.8, 0.9, 0.95, 1.0],
+        'l1_ratio': [0.0, 0.1, 0.2, 0.5, 0.8, 0.9, 0.95, 1.0],
         'loss': ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron', 'squared_loss', 'huber',
                  'epsilon_insensitive', 'squared_epsilon_insensitive'],
         'alpha': [10 ** x for x in range(-6, 1)],
         'random_state': [0]
     }
 
-    opt = BayesSearchCV(model, model_param, n_iter=32, cv=3)
-    opt.fit(train_features, train_classlabels)
-    opt_pred_values = opt.predict(valid_features)
-
-    clfrep = classification_report(valid_classlabels, opt_pred_values)
-    print(clfrep)
-
-
-    print(opt_pred_values)
-    exit()
     valid_f1 = []
     train_f1 = []
-    for l in range(-7, 5):
-        cls = SGDClassifier(random_state=0,
-                                 max_iter=100000,
-                                 class_weight="",
-                                 )
-        cls.fit(train_features, train_classlabels)
-        valid_prediction = cls.predict(valid_features)
-        valid_f1.append(f1_score(valid_classlabels, valid_prediction, pos_label=1))
-        train_prediction = cls.predict(train_features)
-        train_f1.append(f1_score(train_classlabels, train_prediction, pos_label=1))
+    for c in model_param["loss"]:
+        # for d in model_param["alpha"]:
+        for a in model_param["penalty"]:
+            for b in model_param["l1_ratio"]:
 
-    plt.figure(figsize=(10, 10))
-    plt.title("Non Balanced LogReg")
-    plt.plot(valid_f1, c="green")
-    plt.plot(train_f1, c="red")
-    plt.ylabel("F1 score")
-    plt.xlabel("Regularizációs együttható")
-    plt.show()
+                cls = SGDClassifier(random_state=0,
+                                    max_iter=100000,
+                                    penalty=a,
+                                    l1_ratio=b,
+                                    loss=c,
+                                    # alpha=d
+                                    )
+                cls.fit(train_features, train_classlabels)
+                valid_prediction = cls.predict(valid_features)
+                valid_f1.append(f1_score(valid_classlabels, valid_prediction, pos_label=1))
+                train_prediction = cls.predict(train_features)
+                train_f1.append(f1_score(train_classlabels, train_prediction, pos_label=1))
 
-    cls = SGDClassifier()
 
-    # a features a tanító adatbázis egyedeinek jellemzőreprezentációja,
-    # ami ugyanolyan hosszú, mint a címkevektor (a célváltozó ami a tanító példa)
-    model = cls.fit(features, labels)
+            plt.figure(figsize=(10, 10))
+            plt.title(f"SGD Classifier penalty:{a} l1_ratio:{b} loss:{c}")
+            plt.plot(valid_f1, c="green")
+            plt.plot(train_f1, c="red")
+            plt.ylabel("F1 score")
+            plt.xlabel("Regularizációs együttható")
+            plt.show()
+            valid_f1 = []
+            train_f1 = []
 
-    prediction = model.predict(features)
-    print(prediction) # a predikció eredménye egy lista az egyedekre predikált címkékkel
-
-    acc = accuracy_score(y_true=labels, y_pred=prediction)
-    print(acc)
-    # print(classification_report(y_true=labels, y_pred=prediction))
-    return round(acc, 3)
+    return 1
 
