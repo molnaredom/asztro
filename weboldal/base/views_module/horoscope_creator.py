2d068b016736363c8549053b7962ef6d7da01dfa
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-06-07 11:00:52 +0200

    DEBUG: Minden horoszkóp készítésekor a fokpercek 10 es számrendszerben voltak megadva 100' = 1fok, de a mi számításainkban 60' = 1fok. Tehát idáig minden horoszkóp pontatlan volt. Ez lett korrigálva

--- weboldal/base/views_module/horoscope_creator.py
+++ weboldal/base/views_module/horoscope_creator.py
@@ -259,9 +259,13 @@ def get_bolygok(chart):
     for key, value in bolygo_objektumok.items():
         if key == "chiron":
             break
+        print(kisegito.bolygo_to_hun(key), get_fokszam(value["position"]))
+        fokszam = float(get_fokszam(value["position"]))
+        tizedesresz = (fokszam-int(fokszam))/10*6
+        korrigalt_fokszam = int(fokszam) + tizedesresz
         bolygok[kisegito.bolygo_to_hun(key)] = {
             "jegy": kisegito.jegy_num_to_hun(str(value["sign"])),
-            "fokszam": get_fokszam(value["position"]),
+            "fokszam": korrigalt_fokszam,
             "retográd": value["retrograde"],
             "gyorsaság": value["speed"]
         }
