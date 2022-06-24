0946614e3d91a75c7c9ae76dfc2e88173249a477
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-06-24 10:14:57 +0200

    UPDATE: Form tesztek hozzáadása

--- weboldal/base/tests.py
+++ weboldal/base/tests.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, AnonymousUser
 from django.test import TestCase,RequestFactory
-from .models import Jegy2, Bolygo2, Haz2, BolygoJegyben2
+from .models import *
+from .forms import *
 
 
 class PostTestCase(TestCase):
@@ -21,33 +22,33 @@ class PostTestCase(TestCase):
 
 
 class CreateObjects(TestCase):
-    def test_haz(self):
+    def test_haz_koveto(self):
         Haz2.objects.create(nevID="1", leiras="",tipus="követő", mundan_jegye="kos")
         haz_1 = Haz2.objects.get(nevID="1")
         self.assertEqual(haz_1.tipus, 'követő')
 
-    def test_jegy(self):
+    def test_jegy_evszak(self):
         Jegy2.objects.create(nevID="kos", leiras="",elem="tűz",
                              minoseg="kardinális", paritas="pozitív",evszak="tavasz"
                              ,uralkodo_bolygo="mars" )
         kos = Jegy2.objects.get(nevID="kos")
         self.assertEqual(kos.evszak, 'tavasz')
 
-    def test_jegy_rossz(self):
-        # todo meg se lenne szabad engedni hogy a tavaszct meg lehessen adni mint lehetőség
-        Jegy2.objects.create(nevID="kos", leiras="",elem="tűz",
-                             minoseg="kardinális", paritas="pozitív",evszak="tavaszc"
-                             ,uralkodo_bolygo="mars" )
+    # def test_jegy_evszak_elirva(self):
+    #     # todo meg se lenne szabad engedni hogy a tavaszct meg lehessen adni mint lehetőség
+    #     Jegy2.objects.create(nevID="kos", leiras="",elem="tűz",
+    #                          minoseg="kardinális", paritas="pozitív",evszak="tavaszc"
+    #                          ,uralkodo_bolygo="mars" )
+    #
+    #     kos = Jegy2.objects.get(nevID="kos")
+    #     self.assert(kos.evszak, 'tavasz')
 
-        kos = Jegy2.objects.get(nevID="kos")
-        self.assertNotEqual(kos.evszak, 'tavasz')
-
-    def test_bolygo(self):
+    def test_bolygo_tipus(self):
         Bolygo2.objects.create(nevID="nap", leiras="", tipus="személyjelölő", pontertek="2")
         nap = Bolygo2.objects.get(nevID="nap")
         self.assertEqual(nap.tipus, 'személyjelölő')
 
-    def test_bolygojegyben(self):
+    def test_bolygojegyben_kosnap_in_bolygojegyek(self):
         Jegy2.objects.create(nevID="kos", leiras="",elem="tűz",minoseg="kardinális",
                              paritas="pozitív",evszak="tavasz", uralkodo_bolygo="mars" )
         kos = Jegy2.objects.get(nevID="kos")
@@ -60,47 +61,52 @@ class CreateObjects(TestCase):
         kosnap = BolygoJegyben2.objects.get(bolygo=nap, jegy=kos)
         self.assertIn(kosnap, bolygojegyek)
 
-    # def test_bolygohazban(self):
-    #     Jegy2.objects.create(nevID="kos", leiras="",elem="tűz",
-    #                          minoseg="kardinális", paritas="pozitív",evszak="tavasz"
-    #                          ,uralkodo_bolygo="mars" )
-    #     kos = Jegy2.objects.get(nevID="kos")
-    #     self.assertEqual(kos.evszak, 'tavasz')
+    def test_quiz_name(self):
+        Quiz.objects.create(name= "quiz teszt", desc= "valami leiras", number_of_questions= 3,
+                            time= 60, valaszlehetosegek_szama=2 )
+        quiz = Quiz.objects.get(id=1)
+        self.assertEqual(quiz.name, 'quiz teszt')
 
-
-import datetime
-
-from django.test import TestCase
-from django.utils import timezone
-
-from .forms import HoroszkopFormGyors
 
 class FormTest(TestCase):
-    def test_renew_form_date_field_label(self):
-        form = HoroszkopFormGyors()
-        print(form)
-        self.assertTrue(form.fields['hely'].label == "")
 
-    def test_renew_form_date_field_help_text(self):
-        form = HoroszkopFormGyors()
-        self.assertEqual(form.fields['tulajdonos_neve'].help_text, '')
+    def test_valid(self):
+        form_data = {"tulajdonos_neve":"proba1 Név",
+                     "idopont": "2001-08-19 16:54",
+                     "hely": "Szolnok",
+                     "neme": "férfi",
+                     "leirasok": "bármi"}
+        form = HoroszkopFormGyors(data=form_data)
+        self.assertTrue(form.is_valid())
 
-    # def test_renew_form_date_in_past(self):
-    #     date = datetime.date.today() - datetime.timedelta(days=1)
-    #     form = RenewBookForm(data={'renewal_date': date})
-    #     self.assertFalse(form.is_valid())
-    #
-    # def test_renew_form_date_too_far_in_future(self):
-    #     date = datetime.date.today() + datetime.timedelta(weeks=4) + datetime.timedelta(days=1)
-    #     form = RenewBookForm(data={'renewal_date': date})
-    #     self.assertFalse(form.is_valid())
-    #
-    # def test_renew_form_date_today(self):
-    #     date = datetime.date.today()
-    #     form = RenewBookForm(data={'renewal_date': date})
-    #     self.assertTrue(form.is_valid())
-    #
-    # def test_renew_form_date_max(self):
-    #     date = timezone.localtime() + datetime.timedelta(weeks=4)
-    #     form = RenewBookForm(data={'renewal_date': date})
-    #     self.assertTrue(form.is_valid())
+    def test_helytelen_nem(self):
+        form_data = {"tulajdonos_neve":"proba1 Név",
+                     "idopont": "2001-08-19 16:54",
+                     "hely": "Szolnok",
+                     "neme": "férfiak",
+                     "leirasok": ""}
+        form = HoroszkopFormGyors(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_helytelen_idoformatum(self):
+        form_data = {"tulajdonos_neve":"proba1 Név",
+                     "idopont": "2001.08.19. 16:54",  # !!!
+                     "hely": "Szolnok",
+                     "neme": "férfi",
+                     "leirasok": ""}
+        form = HoroszkopFormGyors(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_helytelen_varos(self):
+        """
+        Egyenlőre helyes ha rossz még bele kell építeni,
+         hogy csak a megadott városokat fogadhassa el
+        """
+        form_data = {"tulajdonos_neve": "proba1 Név",
+                     "idopont": "2001-08-19 16:54",
+                     "hely": "otthon",  # !!!
+                     "neme": "férfi",
+                     "leirasok": ""}
+        form = HoroszkopFormGyors(data=form_data)
+        self.assertFalse(form.is_valid())
+
